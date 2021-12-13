f97d0cc44f9594ad98c47e3aab41c7266ac9156f
Uchida Mizuki <35052925+UchidaMizuki@users.noreply.github.com>
2021-12-14 00:13:36 +0900

    Update rectangle.R

--- R/rectangle.R
+++ R/rectangle.R
@@ -5,11 +5,11 @@
 #' @return A \code{mesh} vector.
 #'
 #' @export
-mesh_rectangle <- function(mesh, buffer = 0L) {
+mesh_rectangle <- function(mesh) {
   n_X <- field(mesh, "n_X")
   n_Y <- field(mesh, "n_Y")
-  n_XY <- tidyr::expand_grid(n_X = (min(n_X) - buffer):(max(n_X) + buffer),
-                             n_Y = (min(n_Y) - buffer):(max(n_Y) + buffer))
+  n_XY <- tidyr::expand_grid(n_X = min(n_X):max(n_X),
+                             n_Y = min(n_Y):max(n_Y))
   new_mesh(size = mesh_size(mesh),
            n_X = n_XY$n_X,
            n_Y = n_XY$n_Y)
