29fc8f7b9e39a394ee63322d4bc7a2cb8b447edd
UchidaMizuki <uchidamizuki@vivaldi.net>
2022-11-27 10:45:33 +0900

    Update grid_city2015 data

--- data-raw/grid_city2015.R
+++ data-raw/grid_city2015.R
@@ -1,27 +1,28 @@
+library(tidyverse)
 
 pkgload::load_all()
 
-url_grid_city2015 <- "https://www.stat.go.jp/data/mesh/"
-url_grid_city2015 <- stringr::str_c(url_grid_city2015,
-                                    rvest::read_html(stringr::str_c(url_grid_city2015, "m_itiran.html")) %>%
-                                      rvest::html_elements("#section > article:nth-child(2) > ul > li > a") %>%
-                                      rvest::html_attr("href"))
+# grid_city2015 -----------------------------------------------------------
+
+pref_code <- str_pad(1:47, 2,
+                     pad = "0")
+url_grid_city2015 <- str_glue("https://www.stat.go.jp/data/mesh/csv/{pref_code}.csv")
 
 grid_city2015 <- url_grid_city2015 %>%
-  purrr::map_dfr(function(url_grid_city2015) {
-    readr::read_csv(url_grid_city2015,
-                    locale = readr::locale(encoding = "cp932"),
-                    col_types = readr::cols(.default = "c"),
-                    col_select = 1:3)
-  }) %>%
-  dplyr::rename(city_code = `都道府県市区町村コード`,
+  map_dfr(partial(read_csv,
+                  locale = locale(encoding = "shift-jis"),
+                  col_types = cols(.default = "c"),
+                  col_select = 1:3)) |>
+  rename(city_code = `都道府県市区町村コード`,
                 city_name = `市区町村名`,
-                grid = `基準メッシュコード`) %>%
-  dplyr::mutate(pref_code = city_code %>%
-                  stringr::str_sub(1, 2) %>%
+                grid = `基準メッシュ・コード`) %>%
+  mutate(pref_code = city_code %>%
+                  str_sub(1, 2) %>%
                   as.integer(),
                 grid = grid_1km(grid)) %>%
-  dplyr::relocate(city_code, city_name, pref_code, grid)
+  relocate(city_code, city_name, pref_code, grid) %>%
+  as_tbl_grid(grid,
+              size = "1km")
 
 usethis::use_data(grid_city2015,
                   overwrite = TRUE)
