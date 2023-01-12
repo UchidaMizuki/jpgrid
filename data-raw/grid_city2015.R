53208e1b1a021f21a47847ffd9fea35ef1b437b0
UchidaMizuki <uchidamizuki@vivaldi.net>
2023-01-13 01:11:19 +0900

    Update grid_city_2015

--- data-raw/grid_city2015.R
+++ data-raw/grid_city2015.R
@@ -1,26 +1,50 @@
 library(tidyverse)
+library(rvest)
 
 pkgload::load_all()
 
 # grid_city2015 -----------------------------------------------------------
 
-pref_code <- str_pad(1:47, 2,
-                     pad = "0")
-url_grid_city2015 <- str_glue("https://www.stat.go.jp/data/mesh/csv/{pref_code}.csv")
+pref_name <- rnaturalearth::ne_states(country = "japan",
+                                      returnclass = "sf") |>
+  sf::st_drop_geometry() |>
+  as_tibble() |>
+  select(iso_3166_2, name) |>
+  rename(pref_code = iso_3166_2,
+         pref_name = name) |>
+  mutate(pref_code = pref_code |>
+           str_extract("(?<=JP-)\\d+") |>
+           as.integer())
 
-grid_city2015 <- url_grid_city2015 %>%
-  map_dfr(partial(read_csv,
-                  locale = locale(encoding = "shift-jis"),
-                  col_types = cols(.default = "c"),
-                  col_select = 1:3)) |>
+html_grid_city2015 <- read_html("https://www.stat.go.jp/data/mesh/m_itiran.html") |>
+  html_elements("#section > article:nth-child(2) > ul:nth-child(7) > li > a")
+
+grid_city2015 <- tibble(pref_name_ja = html_grid_city2015 |>
+                          html_text() |>
+                          str_extract("(?<=^\\d{2}\\s).+(?=（)"),
+                        file = html_grid_city2015 |>
+                          html_attr("href") |>
+                          fs::path_file()) |>
+  mutate(pref_code = file |>
+           str_extract("^\\d{2}") |>
+           as.integer(),
+         file = str_glue("https://www.stat.go.jp/data/mesh/csv/{file}")) |>
+  rowwise() |>
+  mutate(grid_city2015 = read_csv(file,
+                                  locale = locale(encoding = "shift-jis"),
+                                  col_types = cols(.default = "c"),
+                                  col_select = 1:3) |>
+           list()) |>
+  ungroup() |>
+  select(!file) |>
+  unnest(grid_city2015) |>
   rename(city_code = `都道府県市区町村コード`,
-                city_name = `市区町村名`,
-                grid = `基準メッシュ・コード`) %>%
-  mutate(pref_code = city_code %>%
-                  str_sub(1, 2) %>%
-                  as.integer(),
-                grid = grid_1km(grid)) %>%
-  relocate(city_code, city_name, pref_code, grid) %>%
+         city_name_ja = `市区町村名`,
+         grid = `基準メッシュ・コード`) |>
+  mutate(grid = grid_1km(grid)) |>
+  left_join(pref_name,
+            by = "pref_code") |>
+  relocate(pref_code, city_code, pref_name, pref_name_ja, city_name_ja, grid) |>
   as_tbl_grid(grid,
               size = "1km")
 
