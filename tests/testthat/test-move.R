d6c0b82c6acc0995b61e059f00b79dd6bb9d6122
UchidaMizuki <uchidamizuki@vivaldi.net>
2023-02-11 00:27:59 +0900

    Update test-move.R

--- tests/testthat/test-move.R
+++ tests/testthat/test-move.R
@@ -1,14 +1,14 @@
 test_that("move", {
-  grid_10km <- grid_parse(533877, "10km")
+  grid_10km <- parse_grid(533877, "10km")
 
-  expect_equal(grid_move(grid_10km, 1, 1), grid_parse(543900, "10km"))
+  expect_equal(move_grid(grid_10km, 1, 1), parse_grid(543900, "10km"))
 })
 
 test_that("neighbor", {
-  grid_10km <- grid_parse(533900, "10km")
+  grid_10km <- parse_grid(533900, "10km")
 
   expect_true(setequal(as.character(grid_neighbor(grid_10km, n = 0:1)[[1]]),
-                       as.character(grid_parse(c(533817, 533910, 533911,
+                       as.character(parse_grid(c(533817, 533910, 533911,
                                                  533807, 533900, 533901,
                                                  523877, 523970, 523971),
                                                grid_size = "10km"))))
