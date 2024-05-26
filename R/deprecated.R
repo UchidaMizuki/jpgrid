775b6f9bdc30c2d3d1d6564e6816dfef9b21e2a1
Uchida Mizuki <uchidamizuki@vivaldi.net>
2024-05-26 17:41:22 +0900

    Deprecate `grid_neighbor()`

--- R/deprecated.R
+++ R/deprecated.R
@@ -1,4 +1,4 @@
-#' Grid square code vector
+#' Grid square code vector (Deprecated)
 #'
 #' `r lifecycle::badge("deprecated")`
 #'
@@ -149,7 +149,7 @@ XY_to_grid <- function(X, Y, grid_size) {
                  grid_size = grid_size)
 }
 
-#' Convert a data frame into a tbl_grid object
+#' Convert a data frame into a tbl_grid object (Deprecated)
 #'
 #' `r lifecycle::badge("deprecated")`
 #'
@@ -271,4 +271,31 @@ plot.tbl_grid <- function(x, y,
 
   plot(x[[grid_column(x)]],
        as_points = as_points, ...)
+}
+
+#' Neighborhood grid square codes (Deprecated)
+#'
+#' `r lifecycle::badge("deprecated")`
+#'
+#' @param grid A `grid` vector.
+#' @param n A numeric vector of degrees. By default, `1L`.
+#' @param moore Moore neighborhood (`TRUE`, default) or Von Neumann neighborhood
+#' (`FALSE`).
+#' @param simplify Should simplify the format of the return?
+#'
+#' @return A list of `grid` vectors.
+#'
+#' @export
+grid_neighbor <- function(grid,
+                          n = 1L,
+                          moore = TRUE,
+                          simplify = TRUE) {
+  lifecycle::deprecate_warn("0.4.0",
+                            what = "grid_neighbor()",
+                            details = 'Please use `grid_neighborhood(type = "moore")`')
+
+  grid_neighborhood(grid,
+                    n = n,
+                    type = if (moore) "moore" else "von_neumann",
+                    simplify = simplify)
 }
